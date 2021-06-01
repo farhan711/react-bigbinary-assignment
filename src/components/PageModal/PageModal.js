8ee6a8cf0d3955539f95659e6fcd60274d2e7100
Farhan <farhan7011@hotmail.com>
2021-06-01 19:03:47 +0530

    Added page modal component

---
+++ src/components/PageModal/PageModal.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Modal } from 'react-bootstrap';
+
+const PageModal = ({ className, header, modalBody, ...props }) => {
+  return (
+    <Modal
+      {...props}
+      aria-labelledby="contained-modal-title-vcenter"
+      className={className}
+      size="lg"
+      centered
+    >
+      {header && (
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">{header}</Modal.Title>
+        </Modal.Header>
+      )}
+      <Modal.Body>{modalBody}</Modal.Body>
+    </Modal>
+  );
+};
+
+PageModal.propTypes = {
+  className: PropTypes.any,
+  header: PropTypes.any,
+  modalBody: PropTypes.any,
+};
+
+export default PageModal;
